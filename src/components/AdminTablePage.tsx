0a2758f2dacb99d7fedf550573837a068f749aa2
fiadaze <fiakantapon@gmail.com>
2025-01-27 15:52:55 +0700

    fix font

--- src/components/AdminTablePage.tsx
+++ src/components/AdminTablePage.tsx
@@ -4,7 +4,7 @@ import AdminReflectionsTable from "./admin-reflections-table";
 export function AdminTablePage() {
   return (
     <div className="flex flex-col gap-2 overflow-hidden p-6">
-      <h3 className="text-5xl font-bold">All User Reflections</h3>
+      <h3 className="text-2xl font-bold">All User Reflections</h3>
       <AdminReflectionsTable />
     </div>
   );
