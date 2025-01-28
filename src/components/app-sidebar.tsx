8ec03af375f74cf706c3b0870289f4022148fceb
fiadaze <fiakantapon@gmail.com>
2025-01-28 12:55:02 +0700

    added path and nav

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -31,6 +31,10 @@ const navigationConfig = {
           title: "AdminPanel",
           url: "/admin",
         },
+        {
+          title: "All Users",
+          url: "/admin/users",
+        },
       ],
     },
   ],
