7508bb05df76df3b32dbe105e488c95be538c675
fiadaze <fiakantapon@gmail.com>
2025-02-04 22:43:33 +0700

    change width last try

--- src/components/emoji-zone-table.tsx
+++ src/components/emoji-zone-table.tsx
@@ -123,7 +123,7 @@ export default function EmojiZoneTable() {
           <TableBody>
             {validDates.map((date) => (
               <TableRow key={date}>
-                <TableCell className="sticky left-0 z-10 w-96 h-12 font-medium border-r ">{date}</TableCell>
+                <TableCell className="sticky left-0 z-10 w-full min-w-[200px] h-12 font-medium border-r">{date}</TableCell>
                 {sortedUsers.map((user) => {
                   const entry = user.entries?.find((e) => e.date === date);
                   const zoneData = entry ? zoneToEmoji[entry.zone] : zoneToEmoji["no-data"];
